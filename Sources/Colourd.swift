403700af2ec00760873232739ab91bfdd5649a49
Tshaka Lekholoane <mail+git@tshaka.dev>
2023-07-03 17:36:41 +0200

    feat: ensure single instance running at a time

--- Sources/Colourd.swift
+++ Sources/Colourd.swift
@@ -3,6 +3,7 @@ import os
 
 @main
 enum Colourd {
+  private static let name = "colourd"
   private static let log = Logger(subsystem: "dev.tshaka.colourd", category: "default")
 
   private static var currentAppearance: String {
@@ -21,7 +22,7 @@ enum Colourd {
         appropriateFor: nil,
         create: true
       )
-      url.append(component: Bundle.main.bundleIdentifier ?? "colourd")
+      url.append(component: Bundle.main.bundleIdentifier ?? name)
       return try FileManager.default.contentsOfDirectory(at: url, includingPropertiesForKeys: nil)
     } catch {
       log.error("\(error.localizedDescription)")
@@ -30,6 +31,12 @@ enum Colourd {
   }
 
   private static var signalSource = DispatchSource.makeSignalSource(signal: SIGTERM, queue: .main)
+
+  private static var isRunning: Bool {
+    // Seems more robust than using lock files.
+    let runningApplications = Set(NSWorkspace.shared.runningApplications.map(\.localizedName))
+    return runningApplications.contains(name)
+  }
 
   /// Registers a `SIGTERM` handler using a `dispatch(3)` source. See
   /// `launchd.plist(5)`.
@@ -67,6 +74,9 @@ enum Colourd {
   }
 
   static func main() {
+    if isRunning {
+      return
+    }
     registerSignalHandlerWithDispatchSource()
     registerNotification()
     log.info("Starting colourd")
